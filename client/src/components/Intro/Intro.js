9901502b38e047a81e4075f12792b31d0e0f6ac6
Mitch Lui <mitch@mitchlui.dev>
2022-04-07 12:21:16 +0100

    new content

--- client/src/components/Intro/Intro.js
+++ client/src/components/Intro/Intro.js
@@ -10,13 +10,15 @@ export const Intro = () =>
             <span className={"container-header header-item"}><h1><code>Hello, I'm Mitch</code></h1></span>
             <span className={"container-header title-item"}><h2>Fullstack software developer</h2></span>
             <div className={"item left"}>
-                Currently working as a Graduate Teacher and 
-                IT Clinic Assistant at the <CompanyLink className={"link uob"} url={"https://www.bristol.ac.uk"} text={"Univeristy of Bristol"} />.
-                Technical Events Officer at <CompanyLink className={"link css"} url={"https://cssbristol.co.uk"} text={"CSS Bristol"} />.
-                Incoming SWE intern at <CompanyLink className={"link wise"} url="https://www.wise.com" text={"Wise"} />.
+                I am a trilingual (English, Cantonese and Mandarin) year 2 computer science student from Hong Kong at the <CompanyLink className={"link uob"} url={"https://www.bristol.ac.uk"} text={"Univeristy of Bristol"} />.
+                Before that, I was an intern at <CompanyLink className={"link asiabots"} url={"https://www.asiabots.com"} text={"Asiabots"} /> writing API-first microservices for omni-channel conversational AIs.
             </div>
             <div className={"item right"}>
-                Previously worked as an intern at <CompanyLink className={"link asiabots"} url={"https://www.asiabots.com"} text={"Asiabots"} /> in Hong Kong writing API-first microservices for omni-channel conversational AIs.
+                Currently working as a Graduate Teacher and 
+                an IT Clinic Technician at the <CompanyLink className={"link uob"} url={"https://www.bristol.ac.uk"} text={"Univeristy"} />,
+                in addition to being the Technical Events Officer at <CompanyLink className={"link css"} url={"https://cssbristol.co.uk"} text={"CSS Bristol"} />.
+                Incoming SWE intern at <CompanyLink className={"link wise"} url="https://www.wise.com" text={"Wise's"} /> Spend Experience
+                Team, where I'll be working on improving their Debit Card product.
             </div>
         </div>
         <Contact/>
