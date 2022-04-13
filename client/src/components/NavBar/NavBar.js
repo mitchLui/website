21b9003486c72c22fddef012bc4b8b0f4e826af9
Mitch Lui <mitch@mitchlui.dev>
2022-04-13 01:28:14 +0100

    mobile nav bar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,18 +1,77 @@
-import React from 'react';
+import { React, useEffect, useState} from 'react';
 import { Link } from "react-router-dom";
 import "./navbar.css";
 
-export const NavBar = () => 
-    <header className="navbar">
-        <Link to={"/"} className="logo-link"><Logo></Logo></Link>
-        <nav>
-            <ul>
-                <li><Link to={"/"}>home</Link></li>
-                <li><Link to={"/projects"}>projects</Link></li>
-                <li><Link to={"/cv.pdf"} target={"_blank"} rel={"noreferrer"}>cv</Link></li>
-            </ul>
-        </nav>
+const MOBILE = 768;
+
+export const NavBar = () => {
+    const [isMobileMenu, setIsMobileMenu] = useState(window.innerWidth < MOBILE);
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth < MOBILE) {
+                setIsMobileMenu(true);
+            } else {
+                setIsMobileMenu(false);
+                setIsOpen(false);
+            }
+        };
+        window.addEventListener("resize", handleResize);
+    });
+
+    return (
+        <>
+        {!isMobileMenu &&  <DesktopNavBar />}
+        {isMobileMenu && <MobileNavBar isOpen={isOpen} setIsOpen={setIsOpen}/>}
+        </>
+    )
+}
+
+export const DesktopNavBar = () => 
+    <header className="navbar desktop">
+        <Link to={"/"} className="logo-link"><Logo /></Link>
+        <NavBarLinks/>
     </header>
+
+export const MobileNavBar = ({isOpen, setIsOpen}) =>
+    <header className="navbar mobile">
+        <div>
+            <Link to={"/"} className="logo-link"><Logo /></Link>
+            <div className="hamburger-menu-container" onClick={() => setIsOpen(!isOpen)}>
+                <HamburgerMenu isOpen={isOpen}/>
+            </div>
+        </div>
+        <div className={`mobile-menu ${isOpen ? "open" : "close"}`}>
+            <NavBarLinks onClick={() => setIsOpen(!isOpen)}/>
+        </div>
+    </header>
+
+export const HamburgerMenu = ({isOpen}) =>
+    <>
+    {
+        !isOpen && 
+        <>
+        menu
+        </>
+    }
+    {
+        isOpen &&
+        <>
+        close
+        </>
+    }
+    </>
+
+export const NavBarLinks = ({onClick}) =>
+    <nav>
+        <ul>
+            <li onClick={onClick}><Link to={"/"}>home</Link></li>
+            <li onClick={onClick}><Link to={"/projects"}>projects</Link></li>
+            <li><Link to={"/cv.pdf"} target={"_blank"} rel={"noreferrer"}>cv</Link></li>
+        </ul>
+    </nav>
+
         
 export const Logo = () => 
     <div className={"logo-container"}>
