34f53d9690a8285307494e66c48f407632a45279
Mitch Lui <mitch@mitchlui.dev>
2022-05-23 18:13:58 +0100

    removed fadein from homepage

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,15 +1,10 @@
 import React from "react";
 import { Contact } from "../../components/Contact/Contact";
 import { Container } from "../../components/Container/Container";
-import { FadeInSection } from "../../components/FadeInSection/FadeInSection";
 import { Intro } from "../../components/Intro/Intro";
 
 export const HomePage = () => 
     <Container>
-        <FadeInSection>
-            <Intro/>
-        </FadeInSection>
-        <FadeInSection>
-            <Contact/>
-        </FadeInSection>
+        <Intro/>
+        <Contact/>
     </Container>
