ee2efe2d8d4d90eb3fdf1c8ad3a918c34d9795d4
Mitch Lui <mitch@mitchlui.dev>
2022-05-23 18:16:09 +0100

    added fadein css

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -2,9 +2,12 @@ import React from "react";
 import { Contact } from "../../components/Contact/Contact";
 import { Container } from "../../components/Container/Container";
 import { Intro } from "../../components/Intro/Intro";
+import "./homepage.css";
 
 export const HomePage = () => 
     <Container>
-        <Intro/>
-        <Contact/>
+        <div className={"fade-in"}>
+            <Intro/>
+            <Contact/>
+        </div>
     </Container>
