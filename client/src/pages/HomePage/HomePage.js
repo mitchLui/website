705cc9edec42a489bb2a921c41f44c69726909cf
Mitch Lui <mitch@mitchlui.dev>
2022-05-23 18:12:55 +0100

    snappier animations

--- client/src/pages/HomePage/HomePage.js
+++ client/src/pages/HomePage/HomePage.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Contact } from "../../components/Contact/Contact";
 import { Container } from "../../components/Container/Container";
+import { FadeInSection } from "../../components/FadeInSection/FadeInSection";
 import { Intro } from "../../components/Intro/Intro";
 
 export const HomePage = () => 
     <Container>
-        <Intro/>
-        <Contact/>
+        <FadeInSection>
+            <Intro/>
+        </FadeInSection>
+        <FadeInSection>
+            <Contact/>
+        </FadeInSection>
     </Container>
