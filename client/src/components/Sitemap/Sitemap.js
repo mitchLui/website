e00cc4d83e26c017c51e4b0c3be20dee44dd65db
Mitch Lui <mitch@mitchlui.dev>
2022-06-06 16:42:13 +0100

    removed meta tags

--- client/src/components/Sitemap/Sitemap.js
+++ client/src/components/Sitemap/Sitemap.js
@@ -1,37 +1,27 @@
 import { React } from 'react';
 import { Link } from 'react-router-dom';
-import DocumentMeta from 'react-document-meta';
 import "./sitemap.scss";
 
-const meta = {
-    title: 'Sitemap',
-    description: 'Sitemap',
-    canonical: 'https://www.mitchlui.dev/sitemap',
-    meta: {
-        charset: 'utf-8',
-    }
-};
-
 export const Sitemap = ({ projects }) =>
-    <DocumentMeta {...meta}>
-    <span className={"container-header"}><h1><code>Sitemap</code></h1></span>
-        <ul className={"sitemap"}>
-            <li><Link to={"/"}>index</Link></li>
-            <ul className={"list"}>
-                <li><Link to={"/projects"}>projects</Link></li>
-                <ul className={"projects"}>
-                    {
-                        Object.keys(projects).map(function(key, index) {
-                            return (
-                                <li key={index}><Link to={"/projects/"+key}>{key}</Link></li>
-                            )
-                        })
-                    }
+    <>
+        <span className={"container-header"}><h1><code>Sitemap</code></h1></span>
+            <ul className={"sitemap"}>
+                <li><Link to={"/"}>index</Link></li>
+                <ul className={"list"}>
+                    <li><Link to={"/projects"}>projects</Link></li>
+                    <ul className={"projects"}>
+                        {
+                            Object.keys(projects).map(function(key, index) {
+                                return (
+                                    <li key={index}><Link to={"/projects/"+key}>{key}</Link></li>
+                                )
+                            })
+                        }
+                    </ul>
+                    <li><Link to={"/cv.pdf"}>cv.pdf</Link></li>
+                    <li><Link to={"/sitemap"}>sitemap</Link></li>
+                    <li><Link to={"/linkedin"}>linkedin</Link></li>
+                    <li><Link to={"/github"}>github</Link></li>
                 </ul>
-                <li><Link to={"/cv.pdf"}>cv.pdf</Link></li>
-                <li><Link to={"/sitemap"}>sitemap</Link></li>
-                <li><Link to={"/linkedin"}>linkedin</Link></li>
-                <li><Link to={"/github"}>github</Link></li>
             </ul>
-        </ul>
-    </DocumentMeta>
+    </>
