7d9a3ecaeeca1b8272160366076d7beb6628d932
Mitch Lui <mitch@mitchlui.dev>
2022-06-25 23:14:17 +0100

    moved client-new into client

--- client/src/components/Arrow/Arrow.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./arrow.scss";
-
-export const Arrow = ({ text }) =>
-    <div className={"arrow-container"}>
-        <div className={"arrow"}/>
-        <span className={"arrow-text"}>{text}</span>
-    </div>
