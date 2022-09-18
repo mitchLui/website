1d35b0ad6a975ccbc29023568de0cb8d2edace6f
Mitch Lui <mitch@mitchlui.dev>
2022-09-18 22:56:39 +0100

    space

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -10,8 +10,7 @@ export const Intro = (): React.ReactElement =>
             Before that, I was an intern at <CompanyLink className={'link asiabots'} url={'https://www.asiabots.com'} text={'Asiabots'} /> writing API-first microservices for omni-channel conversational AIs.
         </div>
         <div className={'item right'}>
-            Currently a Graduate Teacher and IT Clinic Technician / Supervisor at the <CompanyLink className={'link uob'} url={'https://www.bristol.ac.uk'} text={'University'} />
-            in addition to being the Vice-President at <CompanyLink className={'link css'} url={'https://cssbristol.co.uk'} text={'CSS Bristol'} />.
+            Currently a Graduate Teacher and IT Clinic Technician / Supervisor at the <CompanyLink className={'link uob'} url={'https://www.bristol.ac.uk'} text={'University'} /> in addition to being the Vice-President at <CompanyLink className={'link css'} url={'https://cssbristol.co.uk'} text={'CSS Bristol'} />.
             Will be returning as a Graduate Software Engineer Intern at <CompanyLink className={'link wise'} url="https://www.wise.com" text={'Wise\'s'} /> Spend Business Experience Team.
         </div>
     </div>;
