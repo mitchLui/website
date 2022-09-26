a2f85659e55f73894c388587327d95978a3694db
Mitch Lui <mitch@mitchlui.dev>
2022-09-26 11:44:55 +0100

    added dropdown

---
+++ components/Dropdown/Dropdown.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { ComponentMeta } from '@storybook/react';
+
+import Dropdown from '.';
+
+export default {
+  title: 'Dropdown',
+  component: Dropdown,
+} as ComponentMeta<typeof Dropdown>;
+
+export const Primary = () => 
+  <Dropdown
+    htmlFor='dropdown'
+    onChange={(value) => console.log(value)}
+    label='Sort by:'
+    options={{
+      'Number': [
+        {
+          value: '1',
+          text: '1'
+        },
+        {
+          value: '2',
+          text: '2'
+        },
+      ],
+      'Text': [
+        {
+          value: 'a',
+          text: 'a'
+        },
+      ]
+    }}
+  />;
---
+++ components/Dropdown/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import dropdownStyles from './Dropdown.module.css';
+
+type DropdownProps = {
+    htmlFor: string;
+    onChange: (value: string) => void;
+    label: string;
+    options: Record<string, Array<{
+            value: string;
+            text: string
+    }>>;
+}
+
+const Dropdown = ({ htmlFor, onChange, label, options }: DropdownProps): React.ReactElement =>
+  <>
+    <label className={dropdownStyles.label} htmlFor={htmlFor}>{label}</label>
+    <select className={dropdownStyles.menu} onChange={(e) => onChange(e.target.value)} name={'Sort by:'} id={htmlFor} defaultValue={'none'}>
+      {Object.keys(options).map((key) =>
+        <optgroup key={key} label={key}>
+          {options[key].map((option) =>
+            <option key={option.value} value={option.value}>{option.text}</option>
+          )}
+        </optgroup>
+      )}
+    </select>
+  </>;
+
+export default Dropdown;
