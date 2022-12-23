0a367589d521d6efbc57fdf9736f0d3cea890e22
Mitch Lui <mitch@mitchlui.dev>
2022-12-23 15:53:23 +0000

    removed IT Clinic

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -6,11 +6,11 @@ export const Intro = (): React.ReactElement =>
         <span className={'container-header header-item'}><h1><code>Hello, I&apos;m Mitch</code></h1></span>
         <span className={'container-header title-item'}><h2>Fullstack software developer</h2></span>
         <div className={'item left'}>
-            I am a trilingual (English, Cantonese and Mandarin) year 3 computer science student from Hong Kong at the <CompanyLink className={'link uob'} url={'https://www.bristol.ac.uk'} text={'University of Bristol'} />.
+            I am a trilingual (English, Cantonese and Mandarin) 3rd year computer science student from Hong Kong at the <CompanyLink className={'link uob'} url={'https://www.bristol.ac.uk'} text={'University of Bristol'} />.
             Before that, I was an intern at <CompanyLink className={'link asiabots'} url={'https://www.asiabots.com'} text={'Asiabots'} /> writing API-first microservices for omni-channel conversational AIs.
         </div>
         <div className={'item right'}>
-            Currently a Graduate Teacher and IT Clinic Technician / Supervisor at the <CompanyLink className={'link uob'} url={'https://www.bristol.ac.uk'} text={'University'} /> in addition to being the Vice-President at <CompanyLink className={'link css'} url={'https://cssbristol.co.uk'} text={'CSS Bristol'} />.
+            Currently a Graduate Teacher at the <CompanyLink className={'link uob'} url={'https://www.bristol.ac.uk'} text={'University'} /> in addition to being the Vice-President at <CompanyLink className={'link css'} url={'https://cssbristol.co.uk'} text={'CSS Bristol'} />.
             Will be returning as a Graduate Software Engineer at <CompanyLink className={'link wise'} url="https://www.wise.com" text={'Wise\'s'} /> Spend Business Experience Team.
         </div>
     </div>;
