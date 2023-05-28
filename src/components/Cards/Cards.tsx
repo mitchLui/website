420c595a855e0f5b4431e6a9d5defce44e6b6a8f
Mitch Lui <mitch@mitchlui.dev>
2023-05-28 02:24:40 +0100

    Updated Cards Layout

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -14,7 +14,6 @@ type CardProps = {
 
 export const ProjectCard = ({ key, name, title, thumbnail, alt, headline, gitUrl }: CardProps): React.ReactElement =>
     <article key={key} className={'card project ' + name}>
-        <h2>{title}</h2>
         <picture className={'thumbnail'}>
             <source srcSet={'/project_assets/webp/' + thumbnail + '.webp'} type="image/webp" />
             <img
@@ -22,12 +21,13 @@ export const ProjectCard = ({ key, name, title, thumbnail, alt, headline, gitUrl
                 alt={alt}
             />
         </picture>
+        <h2>{title}</h2>
         <div className={'card-content'}>{headline}</div>
         <footer>
             <LinkButton to={'/projects/' + name} text={'Learn more'}/>
             {
                 gitUrl &&
-                <Button url={gitUrl} target={'_blank'} text={'GitHub'}/>
+                <Button url={gitUrl} target={'_blank'} text={'Code'}/>
             }
         </footer>
     </article>;
