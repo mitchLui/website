8ed0e46fd0f48daeb2dbda1e7061b9c46bdee486
Mitch Lui <mitch@mitchlui.dev>
2023-05-29 15:13:00 +0100

    Updated intro and scss

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -2,14 +2,21 @@ import React from 'react';
 import './intro.scss';
 
 export const Intro = (): React.ReactElement =>
-    <div className={'intro-grid'}>
-        <span className={'container-header header-item'}><h1><code>Hello, I&apos;m Mitch</code></h1></span>
-        <span className={'container-header title-item'}><h2>Fullstack software developer</h2></span>
-        <div className={'item left'}>
+    <div className={'intro'}>
+        <span className={'container-header'}><h1><code>Hello, I&apos;m Mitch</code></h1></span>
+        <span className={'container-header'}><h2>Fullstack software developer</h2></span>
+        <div className={'item'}>
+            {/*
+              I'm currently a third-year computer science student at the University of Bristol, based in the UK.
+              Originally from Hong Kong, I can communicate fluently in English, Cantonese, and Mandarin.
+              Alongside my studies, I'm working as a Graduate Teacher at the University.
+              I'm excited to share that I'll be joining Wise's Spend Business Experience Team as a Graduate Software Engineer in the near future.
+              Prior to this, I gained valuable experience as an intern at Asiabots, where I .
+            */}
             I am a UK-based, trilingual (English, Cantonese and Mandarin) 3rd year computer science student from Hong Kong at the <CompanyLink className={'link uob'} url={'https://www.bristol.ac.uk'} text={'University of Bristol'} />.
             Currently a Graduate Teacher at the <CompanyLink className={'link uob'} url={'https://www.bristol.ac.uk'} text={'University'} />.
-            Will be returning as a Graduate Software Engineer at <CompanyLink className={'link wise'} url="https://www.wise.com" text={'Wise\'s'} /> Spend Business Experience Team.
-            Before that, I was an intern at <CompanyLink className={'link asiabots'} url={'https://www.asiabots.com'} text={'Asiabots'} /> writing API-first microservices for omni-channel conversational AIs.
+            Before that, I contributed to the development of API-first microservices for conversational AIs that can be used across multiple channels as an intern at <CompanyLink className={'link asiabots'} url={'https://www.asiabots.com'} text={'Asiabots'} />.
+            I&apos;m excited to be joining <CompanyLink className={'link wise'} url="https://www.wise.com" text={'Wise\'s'} /> Spend Business Experience Team as a Graduate Software Engineer in September.
         </div>
         <div className={'item right'} />
     </div>;
