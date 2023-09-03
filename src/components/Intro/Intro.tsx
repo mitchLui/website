efc5980baa4b41ca938b8ed6677be4ed6f775463
Mitch Lui <mitch@mitchlui.dev>
2023-09-03 23:11:50 +0100

    updated intro

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -6,8 +6,8 @@ export const Intro = (): React.ReactElement =>
         <span className={'container-header'}><h1><code>Hello, I&apos;m Mitch</code></h1></span>
         <span className={'container-header'}><h2>Fullstack software developer</h2></span>
         <div className={'item'}>
-            I am a recent computer science graduate from the <CompanyLink className={'link uob'} url={'https://www.bristol.ac.uk'} text={'University of Bristol'} />. Born and raised in Hong Kong, I am now based in London and will be joining <CompanyLink className={'link wise'} url="https://www.wise.com" text={'Wise\'s'} /> Spend Business Experience Team as a Graduate Software Engineer in September.
-            Prior to university, I contributed to the development of API-first microservices for conversational AIs that can be used across multiple channels as an intern at <CompanyLink className={'link asiabots'} url={'https://www.asiabots.com'} text={'Asiabots'} />.
+        Born and raised in Hong Kong, I am now based in London as a Graduate Software Engineer at <CompanyLink className={'link wise'} url="https://www.wise.com" text={'Wise\'s'} /> Spend Business Team to create the one account for businesses to thrive everywhere.
+        Prior to university, I contributed to the development of API-first microservices for conversational AIs that can be used across multiple channels as an intern at <CompanyLink className={'link asiabots'} url={'https://www.asiabots.com'} text={'Asiabots'} />.
         </div>
         <div className={'item right'} />
     </div>;
