e2c48e5052d8f2ec12eeaffe208c4fb59b07d640
Mitch Lui <mitch@mitchlui.dev>
2023-11-07 21:41:49 +0000

    fix: ahhhhh

--- src/pages/Derrianne/Derrianne.tsx
+++ src/pages/Derrianne/Derrianne.tsx
@@ -15,7 +15,7 @@ export function Derrianne (): React.ReactElement {
   return (
     <Container>
       <h1>To my Standing Goose:</h1>
-      <p>Thanks for being there for me whenever I need you.</p>
+      <p>Thanks for being there for me whenever I need you. I love you! &lt;3</p>
       <img src={'/images/standing-goose.jpeg'} alt={'penguin-plushie-patting-dog-plushie'} width={300}/>
     </Container>
   );
