b2ef277b7f00f3faf4a142f076cdce1a1d494584
Mitch Lui <mitch@mitchlui.dev>
2024-11-03 00:24:59 +0000

    update intro

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -3,7 +3,7 @@ import './intro.scss';
 
 export const Intro = (): ReactElement =>
   <div className={'intro'}>
-    <span className={'container-header'}><h1><code>Hello, I&apos;m Mitch</code></h1></span>
+    <span className={'container-header'}><h1><code>Hello</code></h1></span>
     <span className={'container-header'}><h2>Fullstack software engineer</h2></span>
     <div className={'item'}>
         Born and raised in Hong Kong, I am now based in London as a Graduate Software Engineer at <CompanyLink className={'link wise'} url="https://www.wise.com" text={'Wise\'s'} /> Business Account Management Team to create the one account for businesses to thrive everywhere.
