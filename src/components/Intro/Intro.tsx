0f1d32735b94abe952ff5fe0cf24b363f69d3b8a
Mitch Lui <mitch@mitchlui.dev>
2025-03-22 23:42:03 +0000

    chore: improve intro

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from 'react';
+import { AnchorHTMLAttributes, ReactElement } from 'react';
 import './intro.scss';
 
 export const Intro = (): ReactElement =>
@@ -6,17 +6,11 @@ export const Intro = (): ReactElement =>
     <span className={'container-header'}><h1><code>Hello</code></h1></span>
     <span className={'container-header'}><h2>Fullstack software engineer</h2></span>
     <div className={'item'}>
-        Born and raised in Hong Kong, I am now based in London as a Graduate Software Engineer at <CompanyLink className={'link wise'} url="https://www.wise.com" text={'Wise\'s'} /> Business Account Management Team to create the one account for businesses to thrive everywhere.
-        Prior to university, I contributed to the development of API-first microservices for conversational AIs that can be used across multiple channels as an intern at <CompanyLink className={'link asiabots'} url={'https://www.asiabots.com'} text={'Asiabots'} />.
+        Born and raised in Hong Kong, I am now based in London as a Graduate Software Engineer at <CompanyLink className={'link wise'} href="https://www.wise.com">Wise&apos;s</CompanyLink> Business Account Management Team to create the one account for businesses to thrive everywhere.
+        Prior to university, I contributed to the development of backend APIs for conversational AIs that can be used across voidce and chat as an intern at <CompanyLink className={'link asiabots'} href={'https://www.asiabots.com'}>Asiabots</CompanyLink>.
     </div>
     <div className={'item right'} />
   </div>;
 
-type CompanyLinkProps = {
-    className: string;
-    url: string;
-    text: string;
-};
-
-const CompanyLink = ({ className, url, text }: CompanyLinkProps): ReactElement =>
-  <a className={className} target={'_blank'} rel={'noreferrer'} href={url}>{text}</a>;
+const CompanyLink = ({ className, href, children }: AnchorHTMLAttributes<HTMLAnchorElement>): ReactElement =>
+  <a className={className} target={'_blank'} rel={'noreferrer'} href={href}>{children}</a>;
