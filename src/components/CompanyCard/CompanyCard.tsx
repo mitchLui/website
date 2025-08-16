d43f4fb958a73cdb68ff38c025344da59aafa092
Mitch Lui <mitch@mitchlui.dev>
2025-08-16 11:47:02 +0100

    chore: add motion

--- src/components/CompanyCard/CompanyCard.tsx
+++ src/components/CompanyCard/CompanyCard.tsx
@@ -1,5 +1,6 @@
 import { ReactElement } from 'react';
 import { Company } from '../../data/companies';
+import { motion } from 'motion/react';
 
 import styles from './CompanyCard.module.css';
 import { Link } from 'react-router';
@@ -11,12 +12,14 @@ interface CompanyCardProps {
 
 export const CompanyCard = ({ name, details }: CompanyCardProps): ReactElement => {
   return (
-    <Link className={styles.card} style={{ backgroundColor: details.backgroundColor }} to={'/' + name}>
-      <img
-        className={styles.logo}
-        src={'/company-logos/' + details.logo}
-        alt={details.name}
-      />
-    </Link>
+    <motion.div whileTap={{ scale: 0.95 }}>
+      <Link className={styles.card} style={{ backgroundColor: details.backgroundColor }} to={'/' + name}>
+        <img
+          className={styles.logo}
+          src={'/company-logos/' + details.logo}
+          alt={details.name}
+        />
+      </Link>
+    </motion.div>
   );
 };
