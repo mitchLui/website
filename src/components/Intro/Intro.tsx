5009aa2090fe61ab49e08ae28e5f9274692783f6
Mitch Lui <mitch@mitchlui.dev>
2026-06-01 22:09:45 +0100

    chore: update position

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -3,7 +3,7 @@ import './Intro.scss';
 
 export const Intro = (): ReactElement =>
   <div className={'intro'}>
-      Born and raised in Hong Kong, I am now based in London as a Software Engineer at <CompanyLink className={'wise'} href="https://www.wise.com">Wise&apos;s</CompanyLink> Business Account Management Team to help create the <b>one account for businesses to thrive everywhere</b>.
+      Born and raised in Hong Kong, I am now based in London as a Senior Software Engineer at <CompanyLink className={'wise'} href="https://www.wise.com">Wise&apos;s</CompanyLink> Business Account Management Team to help create the <b>one account for businesses to thrive everywhere</b>.
   </div>;
 
 const CompanyLink = ({ className, href, children }: AnchorHTMLAttributes<HTMLAnchorElement>): ReactElement =>
